118c4c6e04df975eadf36ce9d1903881fd9e9779
expLOIT <herotutos89@gmail.com>
2021-06-17 21:25:59 +0200

    preparing video support :O

--- source/Paths/Paths.hx
+++ source/Paths/Paths.hx
@@ -85,6 +85,12 @@ class Paths
 		return getPath('music/$key.$SOUND_EXT', MUSIC, library);
 	}
 
+	inline static public function video(key:String, ?library:String)
+	{
+		trace('contents/videos/$key.mp4');
+		return getPath('videos/$key.mp4', BINARY, library);
+	}
+
 	inline static public function voices(song:String)
 	{
 		return 'contents/songs/${song.toLowerCase()}/Voices.$SOUND_EXT';
