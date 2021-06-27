7d53d8ee6729ec9b8ec8939e91db4aeb57de6d25
expLOIT <herotutos89@gmail.com>
2021-06-27 16:09:19 +0200

    Unused imports

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -1,17 +1,10 @@
 package;
 
-import Controls.Control;
-import PlayState;
-import flash.text.TextField;
 import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.addons.display.FlxGridOverlay;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.input.keyboard.FlxKey;
-import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import lime.utils.Assets;
 
 class OptionsMenu extends MusicBeatState
 {
